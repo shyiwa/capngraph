c00075a0c0c21d1314503aad2d5dfb20d2b955ed
J David Smith <emallson@atlanis.net>
2016-11-10 15:04:01 -0500

    Added experimental write_graph fn.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate capnp;
 extern crate petgraph;
 
 use petgraph::Graph;
+use petgraph::visit::EdgeRef;
 
 pub mod graph_capnp {
     include!(concat!(env!("OUT_DIR"), "/graph_capnp.rs"));
@@ -9,7 +10,7 @@ pub mod graph_capnp {
 use graph_capnp::weighted_directed_graph as graph;
 use capnp::serialize_packed;
 
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter, Error};
 use std::fs::File;
 
 pub fn load_graph(p: &str) -> capnp::Result<Graph<(), f32>> {
@@ -23,6 +24,31 @@ pub fn load_graph(p: &str) -> capnp::Result<Graph<(), f32>> {
         .map(| edge | (edge.get_from(), edge.get_to(), edge.get_weight()));
 
     Ok(Graph::from_edges(edges))
+}
+
+pub fn write_graph(p: &str, tag: &str, g: &Graph<(), f32>) -> Result<(), Error> {
+    let f = try!(File::create(p));
+    let mut writer = BufWriter::new(f);
+
+    // building the capnp message
+    let mut message = ::capnp::message::Builder::new_default();
+
+    /* build graph */ {
+        let mut graph = message.init_root::<graph::Builder>();
+        graph.set_tag(tag);
+        graph.set_num_nodes(g.node_count() as u32);
+
+        /* collect edges */ {
+            let mut edges_msg = graph.borrow().init_edges(g.edge_count() as u32);
+            for (i, edgeref) in g.edge_references().enumerate() {
+                let mut edge = edges_msg.borrow().get(i as u32);
+                edge.set_from(edgeref.source().index() as u32);
+                edge.set_to(edgeref.target().index() as u32);
+                edge.set_weight(*edgeref.weight());
+            }
+        }
+    }
+    serialize_packed::write_message(&mut writer, &message)
 }
 
 #[cfg(test)]
